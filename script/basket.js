6d40bc39483f25f98f1e96b2325c2d05c6fcd0f8
annaarz <81868053+annaarz@users.noreply.github.com>
2023-12-03 18:38:39 +0300

    Update basket.js

--- script/basket.js
+++ script/basket.js
@@ -118,13 +118,19 @@ const openAndCloseMobileOrder = (disley, px, borderRadius1, borderRadius2) => {
 
 
 if (window.innerWidth <= 1014) {
-    window.addEventListener('click', (e) => {
-        if (e.target.closest('.basket__top')) {
-            openAndCloseMobileOrder('block', 20, '0 0 18px 18px', '18px 18px 0 0')
-        }
-        if (e.target === basketCloseMobile) {
-            openAndCloseMobileOrder('none', 0, '18px', '18px')
-        }
+    // window.addEventListener('click', (e) => {
+    //     if (e.target.closest('.basket__top')) {
+    //         openAndCloseMobileOrder('block', 20, '0 0 18px 18px', '18px 18px 0 0')
+    //     }
+    //     if (e.target === basketCloseMobile) {
+    //         openAndCloseMobileOrder('none', 0, '18px', '18px')
+    //     }
+    // })
+        basketMobile.addEventListener('touchend',() => {
+        openAndCloseMobileOrder('block', 20, '0 0 18px 18px', '18px 18px 0 0')
+    })
+    basketCloseMobile.addEventListener('touchend',() => {
+        openAndCloseMobileOrder('none', 0, '18px', '18px')
     })
 }
 
